Fixed computation of intensity
Fixed calculation of wave intensity at the screen

computational_ghost_imaging_with_wave-behavior.py
@@ -2,20 +2,22 @@
 """
 Created on Fri Mar 20 19:32:52 2020
 
-d = a/N
 
-     |                                 |
-     |                                 |
-     |                                 +(L,y)
-     |                                 |
-     |                                 |
-     +---------------------------------+(L,d/2)
-(0,0)=----------------L----------------+
-     +---------------------------------+(L,-d/2)
-     |                                 |
-     |                                 |
-     |                                 |
-     |                                 |
+      |                                 |
+      |                                 |
+      |                                 +(L,y)
+      |                                 |
+      |                                 |
+   +  +---------------------------------+(L,d/2)
+ D |  +----------------L----------------+(L,0)
+   +  +---------------------------------+(L,-d/2)
+      |                                 |
+      |                                 |
+      |                                 |
+      |                                 |
+
+d = D/N
+
 @author: Efren A. Serra
 """
 
@@ -25,12 +27,12 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-N : int = 1000
+N : int = 100
 # number of discrete points
 
-D : float = .2 * 10 ** -6
+D : float = 6.e-4
 # slit width [micron]
-Y : float = 1.0
+Y : float = 1.
 # Screen width [m]
 
 Dx : float = D / N
@@ -40,31 +42,39 @@ Dy : float = Y / N
 L : int = 100.
 # Distance to screen
 
-LAMBDA : float = 650. * 10 ** -9
+LAMBDA : float = 650.e-9
 k : float = (2. * math.pi) / LAMBDA
 # Units: nm
 # Red lasers typical wavelengths are: 630, 650 and 670 nm
 THETA : float = LAMBDA/D
 
-def Intensity(L : float, k : float, x: object, y: object):
-    nslits = x.size
-    I = np.array([0. for i in range(nslits)])
+def E(L : float, k : float, x: object, y: object):
+    nmslits = x.size
+    E_real = np.array([0. for i in range(nmslits)])
+    E_imag = np.array([1j*0. for i in range(nmslits)])
+
     for j, yj in np.ndenumerate(y):
         for yk in np.nditer(x):
-            delta_kj = math.sqrt(L**2 + (yj-yk)**2)
-            I[j] = I[j] + math.cos(k*delta_kj)**2
+            delta_kj = math.sqrt(L ** 2 + (yj-yk) ** 2)
+            E_real[j] += math.cos(k*delta_kj)
+            E_imag[j] += 1j*math.sin(k*delta_kj)
 
-    return I/nslits
+    return E_real+E_imag
+
+def Intensity(L : float, k : float, x: object, y: object):
+    Ey = E(L, k, x, y)
+
+    return (Ey * np.conj(Ey))/np.sqrt(np.vdot(Ey, Ey))
 
 def main():
     x = np.array([i for i in np.arange(-D/2, D/2, Dx)])
     # Mini-slit points
-    y = np.array([i for i in np.arange(-1./2, 1./2, Dy)])
+    y = np.array([i for i in np.arange(-Y/2, Y/2, Dy)])
     # Screen points
 
     Iy = Intensity(L, k, x, y)
-    plt.title('Single-slit diffraction pattern; No. mini-slits: %d\nSlit widht: 0.2 [micron]'%(N,))
-    plt.plot(y[450:550], Iy[450:550], '-x')
+    plt.title('Single-slit diffraction pattern; no. mini-slits: %d\nSlit width: %.2e [m]; $\lambda$: %e [m]'%(N,D,LAMBDA))
+    plt.plot(y, Iy, '-x')
     plt.xlabel("y [m]")
     plt.ylabel('Normalized\nIntensity')
 