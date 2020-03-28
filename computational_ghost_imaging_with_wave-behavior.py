4ef4fd686d6a2519bb551bace21206d5c6822932
Efren A. Serra <26773123+heavydrizzle@users.noreply.github.com>
2020-03-28 16:57:23 -0700

    Updates to sources and docs
    Latest source code and docs for follow up tasks

--- computational_ghost_imaging_with_wave-behavior.py
+++ computational_ghost_imaging_with_wave-behavior.py
@@ -26,17 +26,18 @@ from __future__ import print_function
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import numexpr as ne
 
 N : int = 100
 # number of discrete points
 
-D : float = 6.e-4
+D : float = 1.4e-3
 # slit width [micron]
 Y : float = 1.
 # Screen width [m]
 
-Dx : float = D / N
-Dy : float = Y / N
+dx : float = D / N
+dy : float = Y / N
 # deltas
 
 L : int = 100.
@@ -50,26 +51,31 @@ THETA : float = LAMBDA/D
 
 def E(L : float, k : float, x: object, y: object):
     nmslits = x.size
-    E_real = np.array([0. for i in range(nmslits)])
-    E_imag = np.array([1j*0. for i in range(nmslits)])
+#    E_real = np.array([0. for i in range(nmslits)])
+#    E_imag = np.array([1j*0. for i in range(nmslits)])
+    wavefront = np.zeros(nmslits, dtype=np.complex128)
 
     for j, yj in np.ndenumerate(y):
         for yk in np.nditer(x):
             delta_kj = math.sqrt(L ** 2 + (yj-yk) ** 2)
-            E_real[j] += math.cos(k*delta_kj)
-            E_imag[j] += 1j*math.sin(k*delta_kj)
+#            E_real[j] += math.cos(k*delta_kj)
+#            E_imag[j] += 1j*math.sin(k*delta_kj)
+            wavefront[j] += (math.cos(k*delta_kj) + 1j*math.sin(k*delta_kj))
 
-    return E_real+E_imag
+#    return E_real+E_imag
+    return wavefront
 
 def Intensity(L : float, k : float, x: object, y: object):
     Ey = E(L, k, x, y)
 
-    return (Ey * np.conj(Ey))/np.sqrt(np.vdot(Ey, Ey))
+#    return (Ey * np.conj(Ey))/np.sqrt(np.vdot(Ey, Ey))
+#    return ne.evaluate("real(abs(Ey))**2")
+    return np.abs(Ey) ** 2
 
 def main():
-    x = np.array([i for i in np.arange(-D/2, D/2, Dx)])
+    x = np.array([i for i in np.arange(-D/2, D/2, dx)])
     # Mini-slit points
-    y = np.array([i for i in np.arange(-Y/2, Y/2, Dy)])
+    y = np.array([i for i in np.arange(-Y/2, Y/2, dy)])
     # Screen points
 
     Iy = Intensity(L, k, x, y)
